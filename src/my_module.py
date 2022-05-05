41056a25cc43ff2c364f5e632314fc131817accf
Karlos Daniel Pires da Silva <karlosdanielsilva7654@gmail.com>
2022-05-05 17:33:43 -0300

    feat: Update my_module.py
    Add the function `extract_days_of_week_from_from_dates(dates)` to my
    `my_module.py`. This function return the list of extracted days from the
    given sequence of dates separeted by commas.

--- src/my_module.py
+++ src/my_module.py
@@ -1,3 +1,23 @@
-def get_cheapest_hotel(number):   #DO NOT change the function's name
+# For debuggin purposes.
+def main():
+    print(extract_days_of_week_from_dates('Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)'))
+    print(extract_days_of_week_from_dates('Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)'))
+    print(extract_days_of_week_from_dates('Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)'))
+
+
+def get_cheapest_hotel(number: str) -> str:   #DO NOT change the function's name
     cheapest_hotel = "cheapest_hotel_name"
     return cheapest_hotel
+
+
+def extract_days_of_week_from_dates(dates: str) -> list[str]:
+    extracted_days_of_week: list[str] = []
+
+    for date in dates.split(','):
+        extracted_days_of_week.append(date.split('(')[1].split(')')[0])
+
+    return extracted_days_of_week
+
+
+if __name__ == '__main__':
+    main()
